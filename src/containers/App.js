f2358b7d143997e31450418b99c4ef50dd5854fb
Anthony <anthonyip@hotmail.fr>
2020-04-30 22:49:48 +0200

    Used jsonplacedholder instead of our data Added Scroll fonctionnality for better UX

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,33 +1,48 @@
 import React, {Component} from 'react';
 import CardList from "../components/CardList";
 import SearchBox from "../components/SearchBox";
-import {robots} from "../robots";
+// import {robots} from "../robots";
 import './App.css';
+import Scroll from "../components/Scroll";
 
 class App extends Component {
     constructor() {
         super();
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: ''
         }
     }
 
+    componentDidMount() {
+        // this.setState({robots : robots})
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then((response) => response.json())
+            .then((users) => this.setState({robots: users}))
+
+    }
+
     onSearchChange = (event) => {
-        this.setState({ searchfield: event.target.value});
+        this.setState({searchfield: event.target.value});
     }
 
     render() {
         const filteredRobots = this.state.robots.filter(robot => {
             return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
         })
-        return (
-            <div className='tc'>
-                <h1>RoboFriends</h1>
-                <SearchBox searchChange={this.onSearchChange}/>
-                <CardList robots={filteredRobots}/>
-            </div>
-        )
+        if (this.state.robots.length === 0) {
+            return <h1>Loading...</h1>
+        } else {
+            return (
+                <div className='tc'>
+                    <h1>RoboFriends</h1>
+                    <SearchBox searchChange={this.onSearchChange}/>
+                    <Scroll>
+                        <CardList robots={filteredRobots}/>
+                    </Scroll>
+                </div>
+            )
+        }
     }
 }
 
